758202c2bdb26640ed2c06d6690ab04552f6051c
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2025-01-19 20:45:01 -0500

    First classes
    En desarrollo las primeras clases

---
+++ src/src/gestorAplicacion/elementos/Cliente.java
@@ -0,0 +1,76 @@
+package gestorAplicacion.elementos;
+
+import gestorAplicacion.elementos.Mascota;
+
+    //import java.io.Serializable;
+
+	// LÓPEZ GONZÁLEZ, ALEJANDRO
+	// BETANCUR URIBE, EMMANUEL
+	// MARTÍNEZ RÍOS, SANTIAGO
+	// BULA FUENTES, MELANIE
+	// OSPINA GAVIRIA, TOMAS
+
+//DESCRIPCIÓN DE LA CLASE:
+//Representa a los usuarios que buscan adoptar animales y que pueden utilizar otros servicios del centro, como la tienda y citas.
+
+
+public class Cliente extends Persona {
+
+	//private static final long serialVersionUID = 1L;
+
+	//ATRIBUTOS	
+
+	private int puntos;
+	private Mascota mascota;
+	
+	//CONSTRUCTOR
+	
+	public Cliente(String nombre,int edad, long cedula,long telefono, String direccion ) {
+		
+		super(nombre,edad,cedula,telefono,direccion);
+	}
+	
+	public Cliente(String nombre, int edad, long cedula) {
+		super(nombre, edad, cedula);
+	}
+	
+	// - - - MÉTODOS - - -
+		
+	//MÉTODOS SETTER Y GETTER
+	
+	public void actualizar_datos(int edad, long telefono, String direccion) {
+		this.setEdad(edad);
+		this.setTelefono(telefono);
+		this.setDireccion(direccion);
+		
+	}
+	
+	public void agregar_puntos(int puntos) {
+		this.puntos+=puntos;
+	}
+	
+	public void disminuir_puntos(int puntos) {
+		this.puntos-=puntos;
+	}
+	
+	public int getPuntos() {
+		return this.puntos;
+	}
+	
+	
+	public void setMascota(Animal mascota){
+		this.mascota=mascota;
+	}
+	
+	public Animal getMascota() { 
+		return mascota;
+	}
+	
+	public String toString() {
+		
+		return "Nombre: " + getNombre() + ", Edad: "+ getEdad()+ ", Cedula: " + getCedula() +", Telefono: " 
+				+ getTelefono() + ", Direccion "+ getDireccion() + "\n";	
+	}
+	
+}
+
---
+++ src/src/gestorAplicacion/elementos/Mascota.java
@@ -0,0 +1,97 @@
+package gestorAplicacion.elementos;
+    
+// LÓPEZ GONZÁLEZ, ALEJANDRO
+// BETANCUR URIBE, EMMANUEL
+// MARTÍNEZ RÍOS, SANTIAGO
+// BULA FUENTES, MELANIE
+// OSPINA GAVIRIA, TOMAS
+
+//DESCRIPCIÓN DE LA CLASE:
+//Representa a los animales disponibles para adopción, incluye atributos como nombre, tipo, edad, sexo y estado de salud, 
+//de igual forma representa a las mascotas que un usuario registre para recibir un servicio o agendar una cita.
+
+public class Mascota {
+
+	// private static final long serialVersionUID = 1L;
+	 public static enum EstadoSalud {
+		SANO, ENFERMO, ENTRATAMIENTO
+	 }
+	
+	private String nombre;
+	private String tipo;
+	private int edad;
+	private String sexo;
+	private EstadoSalud estadoSalud;
+
+	//CONSTRUCTOR
+	
+	public Mascota(String nombre, String tipo, int edad, String sexo, EstadoSalud estadoSalud) {
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.estadoSalud = estadoSalud;
+	}
+	
+	public Mascota(String nombre, String tipo, int edad, String sexo) {
+		this(nombre, tipo, edad, sexo, null);
+	}
+	
+
+	
+	//MÉTODOS SETTER Y GETTER
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEspecie(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public String getEspecie() {
+		return tipo;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	
+	public void setEstadoSalud(EstadoSalud estadoSalud) {
+		this.estadoSalud = estadoSalud;
+	}
+	
+	public EstadoSalud getEstadoSalud() {
+		return estadoSalud;
+	}
+	
+	
+	//OTROS MÉTODOS
+	
+	public String toString() {
+		if (estadoSalud!=null) {
+		return "Nombre: " + getNombre() + ", Especie: " + getEspecie() + ", Edad (meses): " + getEdad() + ", Sexo: " + getSexo() + ", Estado de salud: " + getEstadoSalud();
+		}
+		else {
+			return "Nombre: " + getNombre() + ", Especie: " + getEspecie() + ", Edad (meses): " + getEdad() + ", Sexo: " + getSexo();
+			
+		}
+	}
+}

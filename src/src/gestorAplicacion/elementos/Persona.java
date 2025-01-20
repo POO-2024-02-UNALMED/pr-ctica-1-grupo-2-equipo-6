4dbb8329c83d54f1af114017c37bccde85ec243a
melbulaf <mebulaf@unal.edu.co>
2025-01-19 21:18:08 -0500

    Clases
    Se agregan mas clases.

---
+++ src/src/gestorAplicacion/elementos/Persona.java
@@ -0,0 +1,74 @@
+package gestorAplicacion.elementos;
+
+// BULA FUENTES, MELANIE
+// OSPINA GAVIRIA, TOMAS
+
+//Descripción De La Clase:
+//Representa a las personas vinculadas con el centro de cuidado animal, ya sean clientes o empleados.En esta se almacena información basica de la persona.
+
+public abstract class Persona {
+
+    //---> Atributos <---
+	protected String nombre;
+	protected int edad;
+	protected long cedula;
+	protected long telefono;
+	protected String direccion;
+
+	
+	//---> Constructores <---
+	public Persona(String nombre,int edad, long cedula,long telefono, String direccion) {
+		this(nombre, edad, cedula);
+		this.telefono= telefono;
+		this.direccion= direccion;
+	}
+	
+	public Persona(String nombre,int edad, long cedula) {
+		this.nombre= nombre;
+		this.edad= edad;
+		this.cedula= cedula;
+	}
+	
+	//---> Metodos Getters And Setters <---
+	public void setNombre(String nombre) {
+		this.nombre= nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad= edad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setCedula(long cedula) {
+		this.cedula= cedula;
+	}
+	public long getCedula() {
+		return cedula;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion= direccion;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setTelefono(long telefono) {
+		this.telefono= telefono;
+	}
+	
+	public long getTelefono() {
+		return telefono;
+	}
+	
+	//---> Metodo ToString <---
+	public String toString() {
+		return "Nombre: " + getNombre() + "\nEdad: "+ getEdad()+ "\nCÃ©dula: " + getCedula() +"\nTelefono: " 
+				+ getTelefono() + "\nDirecciÃ³n "+ getDireccion() + "\n";
+	}	
+}
---
+++ src/src/gestorAplicacion/elementos/Producto.java
@@ -0,0 +1,70 @@
+package gestorAplicacion.elementos;
+
+// BULA FUENTES, MELANIE
+// OSPINA GAVIRIA, TOMAS
+
+//Descripción De La Clase:
+//Representa los arti­culos en venta en la tienda, incluyendo nombre, precio, tipo de animal, tipo de Uso y cantidad disponible. 
+
+public class Producto {
+	//---> Atributos <---
+
+    private String nombre; //Nombre del producto.
+    private float precio; //Precio individual del producto.
+    private String tipoAnimal; //Tipo de animal que puede usar el producto.
+    private int cantidadUnidades; //Cantidad de unidades disponibles.
+    private String tipoUso; //Uso al que va orientado el producto.
+
+    //---> Constructores <---
+    public Producto(String nombre, float precio, String tipoAnimal, String tipoUso, int cantidadUnidades){
+        this(nombre, precio, tipoUso, cantidadUnidades);
+        this.tipoAnimal = tipoAnimal;
+    }
+    
+    public Producto(String nombre, float precio, String tipoUso, int cantidadUnidades){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadUnidades = cantidadUnidades;
+    }
+
+    //---> Metodos Getters And Setters <---
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setPrecio(float precio){
+        this.precio = precio;
+    }
+    public float getPrecio(){
+        return this.precio;
+    }
+
+    public void setTipoAnimal(String tipo){
+        this.tipoAnimal = tipo;
+    }
+    public String getTipoAnimal(){
+        return this.tipoAnimal;
+    }
+
+    public void setTipoUso(String tipo){
+        this.tipoUso = tipo;
+    }
+    public String getTipoUso(){
+        return this.tipoUso;
+    }
+
+    public void setCantidadUnidades(int cantidad){
+        this.cantidadUnidades = cantidad;
+    }
+    public int getCantidadUnidades(){
+        return this.cantidadUnidades;
+    }
+
+    //---> Metodo ToString <---
+    public String toString(){
+        return "\nProducto: "+getNombre()+"\n"+"Precio: "+getPrecio()+"\n"+"Usado en: "+getTipoUso()+"\n"+"Proposito: "+getTipoAnimal()+"\n"+ "Cantidad unidades: "+getCantidadUnidades()+"\n";        
+    }
+}
